f64f269ce4ef1d94381bffe148f4753064ef3b46
parimalnajan <63017672+parimalnajan@users.noreply.github.com>
2020-07-28 16:52:51 +0530

    testing pull

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          GIT TEST ASDASDASDSADASDASDASSD         Welcome to <a href="https://nextjs.org">Next.js!</a>
+          GIT TEST 
+          trying to push this daaaaa         Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
